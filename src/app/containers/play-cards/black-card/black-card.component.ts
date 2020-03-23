f5ca58b78691cf0ce3b2377fea171dce80cbc031
Biester Bjoern (PEA3-Fe C/ISP-SVC11) <Bjoern.Biester@de.bosch.com>
2020-03-23 10:02:27 +0100

    alignment corrections

--- src/app/containers/play-cards/black-card/black-card.component.ts
+++ src/app/containers/play-cards/black-card/black-card.component.ts
@@ -2,7 +2,8 @@ import {Component } from '@angular/core';
 
 @Component({
   selector: 'app-black-card',
-  templateUrl: './black-card.component.html'
+  templateUrl: './black-card.component.html',
+  styleUrls: ['./black-card.component.css']
 })
 export class BlackCardComponent {
 
